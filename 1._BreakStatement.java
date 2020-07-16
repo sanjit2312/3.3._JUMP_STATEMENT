40d5e641a01638d02bf716334595c1adabe2531e
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 20:51:27 +0530

    Add files via upload

---
+++ 1._BreakStatement.java
@@ -0,0 +1,19 @@
+package jumpStatement;
+
+public class BreakStatement
+{
+
+	public static void main(String[] args) 
+	{
+
+		for(int i=0;i<10;i++)
+		{
+			if(i==5)
+			{
+				break;
+			}
+			System.out.print(i+" ");
+		}
+	}
+
+}
